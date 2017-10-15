23263b2b7d5c647ffe581168a16b64bcfa9ee90f
Amanuel <yihdego@msn.com>
2017-10-14 21:08:53 -0700

    wrote my association tests for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015020530) do
+ActiveRecord::Schema.define(version: 20171015032926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -22,4 +30,14 @@ ActiveRecord::Schema.define(version: 20171015020530) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "upvotes", force: :cascade do |t|
+    t.boolean "like"
+    t.string "votable_type"
+    t.bigint "votable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["votable_type", "votable_id"], name: "index_upvotes_on_votable_type_and_votable_id"
+  end
+
+  add_foreign_key "comments", "posts"
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  post = Post.create!(title: "Test Post", body: "Filler text")
+  context 'attributes' do
+    it 'has a title' do
+      expect(post.title).to eq "Test Post"
+    end
+    it 'has a body' do
+      expect(post.body).to eq "Filler text"
+    end
+  end
+  context 'associations' do
+    it 'has no comments' do
+      expect(post.comments).to be_empty
+    end
+    it 'has no votes' do
+      expect(post.upvotes).to be_empty
+    end
+    it 'has has comments' do
+      comment = Comment.create(body:"Troll comment", post: post)
+      expect(post.comments).to include(comment)
+    end
+    it 'has has votes' do
+      Upvote.create(like:true, votable: post)
+      Upvote.create(like:false, votable: post)
+      expect(post.upvotes.count).to eq 2
+    end
+  end
 end
