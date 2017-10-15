d03bf625f6da35c5221a81885e986ea3ab51d5f5
Amanuel <yihdego@msn.com>
2017-10-14 21:13:19 -0700

    comments votable

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  post = Post.create!(title: "Test Post", body: "Filler text")
+  comment = Comment.create!(body: "Troll text", post: post)
+  context 'attributes' do
+    it 'has a body' do
+      expect(comment.body).to eq "Troll text"
+    end
+  end
+  context 'associations' do
+    it 'has no votes' do
+      expect(comment.upvotes).to be_empty
+    end
+    it 'has has votes' do
+      Upvote.create(like:false, votable: comment)
+      Upvote.create(like:false, votable: comment)
+      expect(comment.upvotes.count).to eq 2
+    end
+  end
 end
