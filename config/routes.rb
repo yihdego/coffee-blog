240028b7b126264fb1ad15801324faca1acc74d9
yihdego <yihdego@msn.com>
2017-10-14 23:24:20 -0700

    wrote routes for controller actions of post comments and votes and the views as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,45 @@
 Rails.application.routes.draw do
+  get 'upvotes/new'
+
+  get 'upvotes/create'
+
+  get 'upvotes/edit'
+
+  get 'upvotes/update'
+
+  get 'upvotes/destroy'
+
+  get 'comments/index'
+
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'comments/show'
+
+  get 'comments/edit'
+
+  get 'comments/update'
+
+  get 'comments/destroy'
+
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/show'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
+  resources :posts
+  resources :comments
+  resources :upvotes
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
